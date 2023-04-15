3817acbaf94968585b2367a7657ca90e7f7a01c5
Gisele Ribeiro <giselesousar@gmail.com>
2023-04-15 11:51:36 -0300

    fix: adjusting favorite reducer to bind favorites to an user

--- src/redux/reducers/favorites.ts
+++ src/redux/reducers/favorites.ts
@@ -1,20 +1,34 @@
 import { Movie } from "../../models";
 
-const INITIAL_STATE = {
-    movies: [],
+const INITIAL_STATE: { favorites: { username: string; movies: Movie[] }[] } = {
+    favorites: [],
 }
 
 type IAction = {
     type: string;
     movie: Movie
+    username: string;
 }
 
 export default (state = INITIAL_STATE, action: IAction) => {
     switch (action.type) {
         case 'FAVORITE_MOVIE':
-            const filtered = state.movies.filter((movie: Movie) => movie.id !== action.movie.id)
+            const user = state.favorites.find((favorite) => favorite.username === action.username);
+            if (!user) {
+                return {
+                    favorites: [...state.favorites, { username: action.username, movies: [action.movie] }]
+                };
+            }
+            const filtered = user.movies.filter((movie: Movie) => movie.id !== action.movie?.id)
             return {
-                movies: filtered.length < state.movies.length ? filtered : [...state.movies, action.movie]
+                favorites: state.favorites.map(favorite =>
+                    favorite.username === action.username
+                        ? {
+                            ...favorite, movies: filtered.length < favorite.movies.length
+                                ? filtered
+                                : [...favorite.movies, action.movie]
+                        }
+                        : favorite)
             };
         default:
             return state;
