4e87a4148f3ecc1ede26a2a802a4a2a9c0be80f1
Gisele Ribeiro <giselesousar@gmail.com>
2023-04-15 11:52:59 -0300

    feat: creating filters component

---
+++ src/components/SideMenuFilter.tsx
@@ -0,0 +1,135 @@
+import React, {
+  useState,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+  Ref,
+} from "react";
+import {
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useTranslation } from "react-i18next";
+
+import { Genre } from "../models";
+import Checkbox from "./Checkbox";
+
+type SideMenuFilterProps = {
+  genres: Genre[];
+  onSave: (checked: Genre[]) => void;
+};
+
+export type SideMenuHandle = {
+  open: () => void;
+};
+
+function SideMenuFilter(
+  { genres, onSave }: SideMenuFilterProps,
+  ref: Ref<SideMenuHandle>
+) {
+  const [checked, setChecked] = useState<Genre[]>(genres);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { t } = useTranslation();
+
+  useImperativeHandle(ref, () => ({
+    open: () => setIsOpen(true),
+  }));
+
+  const isChecked = useCallback(
+    (id: number) => Boolean(checked.find((genre: Genre) => genre.id === id)),
+    [checked]
+  );
+
+  const check = (id: number, name: string) => {
+    const filtered = checked.filter((genre: Genre) => genre.id !== id);
+    setChecked(
+      filtered.length < checked.length ? filtered : [...checked, { id, name }]
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Modal visible={isOpen} transparent={true}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.menu}>
+            <View style={styles.row}>
+              <Text style={styles.label}>{t("common:genres")}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  onSave(checked);
+                  setIsOpen(false);
+                }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>{t("common:save")}</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.wrapped}>
+              {(genres || []).map((genre: Genre) => (
+                <View key={`genre-${genre.id}`}>
+                  <Checkbox
+                    id={genre.id}
+                    label={genre.name}
+                    isChecked={isChecked(genre.id)}
+                    check={check}
+                  />
+                </View>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      </Modal>
+    </View>
+  );
+}
+
+export default forwardRef<SideMenuHandle, SideMenuFilterProps>(SideMenuFilter);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  menu: {
+    backgroundColor: "#fff",
+    height: "55%",
+    width: "90%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.6,
+    shadowRadius: 5,
+    position: "absolute",
+    borderRadius: 8,
+    padding: 20,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  wrapped: {
+    flexWrap: "wrap",
+    gap: 5,
+    marginTop: 10,
+    paddingBottom: 30,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#eee",
+    padding: 10,
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
---
+++ src/screens/Home/Filters.tsx
@@ -0,0 +1,102 @@
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  Fragment,
+  useRef,
+  Ref,
+} from "react";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useTranslation } from "react-i18next";
+
+import { SideMenuFilter } from "../../components";
+import { Genre } from "../../models";
+import { SideMenuHandle } from "../../components/SideMenuFilter";
+
+type FiltersProps = {
+  refresh: () => void;
+  genres: Genre[];
+  setFilterGenres: (checked: Genre[]) => void;
+};
+
+export type FiltersHandle = {
+  query: () => string;
+};
+
+function Filters(
+  { refresh, genres, setFilterGenres }: FiltersProps,
+  ref: Ref<FiltersHandle>
+) {
+  const [query, setQuery] = useState<string>("");
+
+  const sideMenuFilterRef = useRef<SideMenuHandle>(null);
+
+  const { t } = useTranslation();
+
+  useImperativeHandle(ref, () => ({
+    query: () => query,
+  }));
+
+  useEffect(() => {
+    const delay = setTimeout(refresh, 500);
+    return () => clearTimeout(delay);
+  }, [query]);
+
+  return (
+    <Fragment>
+      <View style={styles.container}>
+        <View style={styles.search}>
+          <FontAwesome5 name="search" size={14} color="#aaa" />
+          <TextInput
+            style={styles.input}
+            placeholder={t("common:search") as string}
+            value={query}
+            onChangeText={setQuery}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => sideMenuFilterRef?.current?.open()}
+          style={styles.filter}
+        >
+          <FontAwesome5 name="filter" size={16} />
+        </TouchableOpacity>
+      </View>
+      <SideMenuFilter
+        ref={sideMenuFilterRef}
+        genres={genres}
+        onSave={setFilterGenres}
+      />
+    </Fragment>
+  );
+}
+
+export default forwardRef<FiltersHandle, FiltersProps>(Filters);
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    marginTop: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  search: {
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    borderColor: "#aaa",
+  },
+  input: {
+    width: "80%",
+    paddingLeft: 8,
+  },
+  filter: {
+    backgroundColor: "#eee",
+    padding: 10,
+    borderRadius: 50,
+  },
+});
